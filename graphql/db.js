e410148a34c4f9028edd4c04f6203e4e6eb49258
jaducku <avenueguy@gmail.com>
2018-08-13 11:24:08 +0900

    Update with axios

--- graphql/db.js
+++ graphql/db.js
@@ -1,17 +1,47 @@
-export const people = [{
-    id : 1,
-    name : "kyle",
-    age : 36,
-    gender : "Female"
-    },
-    {
-    id :2,
-    name : "eunhye",
-    age : 31,
-    gender : "Mail"
-    }];
+import axios from "axios";
 
-export const getById = (id) => {
-    const filteredPeople = people.filter(person => person.id === id);
-    return filteredPeople[0];
+const BASE_URL              = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL       = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL     = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
+
+export const getMovies = async (limit,rating) => {
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(LIST_MOVIES_URL, {
+        params: {
+            limit,
+            minimum_rating: rating
+        }
+    });
+    return movies;
 }
+
+export const getMovie = async (id) =>{
+    const{
+        data: {
+            data:{movie}
+        }
+    } = await axios(MOVIE_DETAILS_URL,{
+        params: {
+            movie_id:id
+        }
+    });
+    return movie;
+}
+
+export const getSuggestions = async (id) =>{
+    const{
+        data: {
+            data:{movies}
+        }
+    } = await axios(MOVIE_SUGGESTIONS_URL,{
+        params: {
+            movie_id:id
+        }
+    });
+    return movies;
+}
+    
